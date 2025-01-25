6cdfbf3c1c367e1197b7bff4808580ddebc079f5
SrinithishB <bsrinithish@gmail.com>
2025-01-25 14:54:04 +0530

    calculator activity added

--- app/src/main/java/com/example/personalassistant/CalculatorActivity.java
+++ app/src/main/java/com/example/personalassistant/CalculatorActivity.java
@@ -1,31 +1,24 @@
 package com.example.personalassistant;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-CardView calculator;
+public class CalculatorActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calculator);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        calculator=findViewById(R.id.calculator_card);
-        calculator.setOnClickListener(v -> {
-            Intent intent=new Intent(this,CalculatorActivity.class);
-            startActivity(intent);
         });
     }
 }
